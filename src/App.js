d7bfae0df3aab43f2559feef074c7bf712182a85
shimizu-mai <lunetta0025@gmail.com>
2024-02-25 00:54:24 +0900

    a

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import { useState } from "react";
 function App() {
   const [num, setNum] = useState(0);
   const countUp = () => {
+    if (num < 10) {
     setNum(num + 1);
+    }
   };
   const countDown = () => {
-    setNum(num - 1);
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+  const reset = () => {
+    setNum(0);
   };
 
   return (
   <><div style={{fontSize:30,fontWeight:"bold"}}>{num}</div>
-  <button onClick={countUp}>+</button>
-  <button onClick={countDown}>-</button>
+  <button onClick={countUp} disabled={num >= 10}>+</button>
+      <button onClick={countDown} disabled={num <= 0}>-</button>
+      <button onClick={reset} disabled={num === 0}>リセット</button>
   </>
   );
 }
